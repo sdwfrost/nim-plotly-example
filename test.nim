e8697cc80ba2da6a615051772e325f9aad3e9318
Simon Frost <sdwfrost@gmail.com>
2017-09-22 18:22:41 +0100

    Updated demo

--- test.nim
+++ test.nim
@@ -1,15 +1,24 @@
 import jsffi
 import plotly
 
-var trace=newJsObject()
-trace.x = @[1, 2, 3, 4]
-trace.y = @[18, 15, 13, 12]
-trace.mode = "lines+markers"
+# Line plot
 
-var data = @[trace]
+var trace1=newJsObject()
+trace1.x = @[1, 2, 3, 4]
+trace1.y = @[18, 15, 13, 12]
+trace1.mode = "lines+markers"
+var data1 = @[trace1]
+var layout1 = newJsObject()
+layout1.title = "Line plot".cstring
+Plotly.newPlot("line-plot", data1, layout1)
 
-var layout = newJsObject()
-layout.title = "Line and Scatter Plot"
+# Bar plot
 
-Plotly.newPlot("myDiv", data, layout)
-
+var trace2 = newJSObject()
+trace2.x = @["giraffes".cstring,"orangutans".cstring,"monkeys".cstring]
+trace2.y = @[20,14,23]
+trace2.type = "bar".cstring
+var data2 = @[trace2]
+var layout2 = newJsObject()
+layout2.title = "Bar plot".cstring
+Plotly.newPlot("bar-plot", data2, layout2)
